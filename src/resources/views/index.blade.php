9f2f538a54ffdbbca164528b807db4ab753026c0
yokotamako <yokotamako858@gmaiil.com>
2025-10-21 00:02:40 +0900

    サンプルファイルの追加

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+@endsection
+
+@section('content')
+    <div class="test_freshly_picked__alert">
+        @if (session('message'))
+            <div class="__alert--success">{{ session('message') }}</div>
+        @endif @if ($errors->any())
+            <div class="test_freshly_picked__alert--danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+    <div class="test_freshly_picked__content">
+        <div class="section__title">
+            <h2>おすすめ</h2><h2>マイリスト</h2>
+        </div>
+        <form class="create-form" action="/" method="post">
+            @csrf
+            <div class="create-form__item">
+                <input class="create-form__item-input" type="text" name="content" + value="{{ old('content') }}" />
+            </div>
+            <div class="create-form__button">
+                <button class="create-form__button-submit" type="submit">検索</button>
+            </div>
+        </form>
+        <div class="section__title">
+            <h2>価格順で表示</h2>
+        </div>
+        <form class="search-form">
+            <div class="search-form__item">
+                <input class="search-form__item-input" type="text" />
+                <select class="search-form__item-select">
+                    <option value="high">高い順に表示</option>
+                    <option value="low">低い順に表示</option>
+                </select>
+            </div>
+        </form>
+        <form action="/" method="post" enctype='multipart/form-data'>
+            <input type="file" name="thumbnail" />
+            <div class="product-grid">
+
+            </div>
+        </form>
+    </div>
+@endsection
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mogitate</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilities">
+                <img src="img/logo.svg" alt="coachtech"><br>
+                <a class="header__logo">
+                    
+            </div>
+        </div>
+        @yield('content')
+        <div class="main">
+
+            <nav>
+                <ul class="main-nav">
+                    <li class="main-nav__item">
+                        <a class="main-nav__link" href="/products">商品を追加
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+    </main>
+</body>
+
+</html>
